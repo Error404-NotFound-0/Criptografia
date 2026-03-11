efd87d7e715f9b4f53942eaf22d0de6672e2745c
Error404-NotFound_ <Arpachelli@gmail.com>
2026-03-10 21:57:37 -0300

    main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import random
+import time
 def main():
+    random.seed(time.time())
     while True:
-        print('-'*10, 'CRIPTOGRAFADOR', '-'*10)
+        print('\n\n\n', '-'*10, 'CRIPTOGRAFADOR', '-'*10)
         e = input(' Opções; \n\n a) Criptografar uma mensagem \n b) Descriptografar uma mensagem \n\n Escolha (a, b): ')
         if e.lower() == 'a':
             criptografar()
@@ -16,17 +19,36 @@ def criptografar():
             print('\n', '-'*10, 'CRIPTOGRAFAR', '-'*10)
             m = input('\n Insira a mensagem que será criptografada: ')
             c = ''
+            chave1 = random.randint(1, 1000)
+            chave2 = random.choice(['a', 'b'])
+            
+
             break
         except:
             print('Algo deu errado, tente novamente.')
             return
     for i in m:
         if i.isalpha():
-            c += f"<{ord(i)}>"
+            if chave2.lower() == 'a':
+                c += f"<{(ord(i)+chave1)*chave1}>"
+            if chave2.lower() == 'b':
+                c += f"<{(ord(i)*chave1)+chave1}>"
         else:
-            c += f"<{i}>"
+            while True:
+                try:
+                    if chave2.lower() == 'a':
+                        c += f"<{(int(i)+chave1)*chave1}>"
+                        break
+                    elif chave2.lower() == 'b':
+                        c += f"<{(int(i)*chave1)+chave1}>"
+                        break
+                except:
+                    c += f"<{i}>"
+                    break
     print('\n', '-'*10, 'MENSAGEM CRIPTOGRAFADA', '-'*10)
     print('\n', c, '\n')
+    print('\n A chave da mensagem é: ', f'{chave1}'+f'{chave2}')
+    recomecar()
 
 
 def descriptografar():
@@ -34,30 +56,51 @@ def descriptografar():
         try:
             print('\n', '-'*10, 'DESCRIPTOGRAFAR', '-'*10)
             m = input('\n Insira a mensagem que será descriptografada: ')
+            chave = input('\n Insira a chave da mensagem: ')
             d = ''
             na_lista = False
             lista = ''
+            chave1 = int(chave[:-1])
             break
         except:
             print('Algo deu errado, tente novamente.')
             return
+
+
     for i in m:
         if i == '<':
             na_lista = True
             lista = ''
         elif i == '>':
             if lista.isdigit():
-                num = int(lista)
-                if (90 >= num >= 65) or (122 >= num >= 97):
+                if chave[-1].lower() == 'b':
+                    num = (int(lista) - chave1) // chave1
+                
+                elif chave[-1].lower() == 'a':
+                    num = (int(lista) // chave1) - chave1
+
+                if (90 >= num >= 65) or (122 >= num >= 97) or num > 122:
                     d += chr(num)
                 else:
                     d += str(num)
             else:
                 d += lista
+            na_lista = False
         elif na_lista:
             lista += i
 
     print('\n', '-'*10, 'MENSAGEM DESCRIPTOGRAFADA', '-'*10)
     print('Mensagem Original: ', ''.join(d))
+    recomecar()
+
+def recomecar():
+    while True:
+        e = input("\n Você deseja fazer novamente? (S/N)")
+        if e.lower() == 's':
+            break
+        if e.lower() == 'n':
+            quit()
+        else:
+            print('Escolha inválida.')
 
 main()
